5d173df5f45411dc2803a6d0b3b2b453c46e9a81
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-12-29 11:46:24 +0100

    Configure GameDataServer class

---
+++ src/main/java/nl/gamedata/server/GameDataServer.java
@@ -0,0 +1,14 @@
+package nl.gamedata.server;
+
+/**
+ * GameDataServer.java.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://github.com/averbraeck/gamedata-server/LICENSE">GameData project License</a>.
+ * </p>
+ * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
+ */
+public class GameDataServer
+{
+
+}
